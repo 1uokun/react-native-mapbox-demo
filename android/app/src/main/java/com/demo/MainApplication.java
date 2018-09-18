671213d0a818b2951aa41443e4c9a5d26585fd7d
罗坤 <1498710037@qq.com>
2018-09-18 12:01:10 +0800

    删除new MyToastPackage(),

--- android/app/src/main/java/com/demo/MainApplication.java
+++ android/app/src/main/java/com/demo/MainApplication.java
@@ -3,14 +3,12 @@ package com.demo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.example.react_native_mytoast.MyToastPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.mapbox.rctmgl.RCTMGLPackage;
-//import com.reactlibrary.RNAmapLocationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-//            new RNAmapLocationPackage(),
-            new MyToastPackage(),
             new VectorIconsPackage(),
             new RCTMGLPackage()
       );
