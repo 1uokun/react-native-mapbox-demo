90cf084839e89fdc5d4c7bef45f8d1dee0735a0f
罗坤 <1498710037@qq.com>
2018-09-18 13:01:05 +0800

    基本地图-添加瓦片加载资源

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -3,10 +3,32 @@ import {View} from 'react-native'
 import MapboxGL from '@mapbox/react-native-mapbox-gl'
 export default class extends React.Component {
     render(){
+        const rasterSourceProps = {
+            id: 'stamenWatercolorSource',
+            url: 'http://www.google.cn/maps/vt?lyrs=s@810&gl=cn&x={x}&y={y}&z={z}',
+            tileSize: 256,
+        };
         return (
             <MapboxGL.MapView
                 style={{flex:1}}
-            />
+                animated={true}
+                localizeLabels={true}
+                showUserLocation={true}
+                logoEnabled={false}
+                rotateEnabled={false}
+                attributionEnabled={false}
+                compassEnabled={false}
+            >
+                {/*瓦片图层（资源来自google.cn）*/}
+                <MapboxGL.RasterSource {...rasterSourceProps}>
+                    <MapboxGL.RasterLayer
+                        layerIndex={100}
+                        id="stamenWatercolorLayer"
+                        sourceID="stamenWatercolorSource"
+                        style={{ rasterOpacity: 1 }}
+                    />
+                </MapboxGL.RasterSource>
+            </MapboxGL.MapView>
         )
     }
 }
